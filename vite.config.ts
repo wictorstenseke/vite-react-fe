d1a179ed24e2c97ca54801d968a27f624f2796d5
Wictor Stenseke <wictorstenseke@gmail.com>
2025-12-16 19:55:58 +0100

    added agnostig deployment configs

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite(), tailwindcss()],
+  base: process.env.BASE_PATH || "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
